31c263d8507e4cb8e210b4c4d9ca66cb6493f40d
andikatuluspangestu <andikatulusp@gmail.com>
2024-01-28 12:28:27 +0700

    Changes : Format Code agar Rapih

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,16 +11,12 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         $i = [];
+
         // Menghitung Performa cafe
-        foreach(Cafe::latest()->where('konfirmasi', 'konfirmasi')->get() as $cafe){
+        foreach (Cafe::latest()->where('konfirmasi', 'konfirmasi')->get() as $cafe) {
             $i[] = [($cafe->ulasan->sum('rating') + $cafe->beli->where('no_pesanan', '!==', null)->sum('jumlah')), $cafe->nama_cafe];
         }
 
@@ -29,11 +25,9 @@ class HomeController extends Controller
 
         // Ambil Data
         $a = [];
-        foreach($i as $b){
+        foreach ($i as $b) {
             $a[] = $b[1];
         }
-
-
 
         return view('index', [
             'cafes' => Cafe::latest()->where('konfirmasi', 'konfirmasi')->cari(request(['cari']))->get(),
