58a6cf910c1dc0a259d88d480063f02981921d47
andikatuluspangestu <andikatulusp@gmail.com>
2024-01-29 10:45:15 +0700

    Fixed : Margin

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -39,7 +39,7 @@
 
     @include('partials.navbar')
     @include('partials.textHeader')
-    <div class="container">
+    <div class="container my-5">
         @include('sweetalert::alert')
         @yield('container')
     </div>
