6a96e75abcc7859f40d34b0b4bd7d383ea597f38
andikatuluspangestu <andikatulusp@gmail.com>
2024-01-30 14:05:16 +0700

    Added : Help Page VIew

---
+++ resources/views/pages/help.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.main')
+
+@section('container')
+    <section class="bantuan">
+        <div class="page-header">
+            <h2 class="page-title">
+                Halaman Bantuan
+            </h2>
+            <p class="page-desc">
+                Berisi tentang bantuan yang dapat anda akses untuk mempermudah anda dalam menggunakan
+                aplikasi ini.
+            </p>
+        </div>
+        <div class="page-content card rounded-sm p-4 border-0 shadow-sm">
+            <div class="accordion accordion-flush" id="accordionFlushExample">
+
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingOne">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#cara-pesan"
+                            aria-expanded="false" aria-controls="cara-pesan">
+                            Cara Pesan
+                        </button>
+                    </h2>
+                    <div id="cara-pesan" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
+                        data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">
+                            a. User harus daftar ke webnya dahulu,<br>
+                            b. Setelah itu user akan automatis login ke webnya,<br>
+                            c. Pilih café yang ingin dikunjungi,<br>
+                            d. Didalam halaman café, bisa menuju ke bagian menu,<br>
+                            e. User bebas memilih menu yang ingin mereka pesan dengan klik tombol “beli”,<br>
+                            f. Ketika ingin melihat detail pesanan, user bisa menekan tombol “lihat pesanan”,<br>
+                            g. Setelah itu langsung menuju proses pembayaran. <br>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#cara-booking" aria-expanded="false" aria-controls="cara-booking">
+                            Cara Booking
+                        </button>
+                    </h2>
+                    <div id="cara-booking" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo"
+                        data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">
+                            <ol style="text-align: left;">
+                                <li>User mendaftar akun dan akan automatis login kedalam webnya,</li>
+                                <li>&nbsp;Pilih café yang ingin dikunjungi,</li>
+                                <li>Didalam halaman café, bisa menuju ke bagian reservasi,</li>
+                                <li>Disini user bisa memilih tempat yg ingin direservasi sesuai keinginan mereka,</li>
+                                <li>Klik tombol “booking” untuk memulai proses reservasi,</li>
+                                <li>Lalu Isi tanggal booking dan jam bookingnya,</li>
+                                <li>Klik “Lanjut booking”,</li>
+                                <li>Setelah itu user akan dialihkan ke halaman detail reservasi. Disini user bisa membayar reservasi
+                                    dengan
+                                    klik tombol “reservasi” dan membatalkan reservasi dengan klik “hapus”,</li>
+                                <li>Lalu setelah klik tombol “bayar”, user bisa langsung transaksi reservasi melalui web ini
+                                    langsung atau
+                                    menghubungi pihak cafenya,</li>
+                                <li>Setelah kirim bukti kedalam web, user harus menunggu status reservasi,</li>
+                                <li>Jika sukses maka status akan berubah menjadi sukses.</li>
+                            </ol><br />
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#cara-mendaftar-cafe" aria-expanded="false" aria-controls="cara-mendaftar-cafe">
+                            Cara Mendaftar Cafe
+                        </button>
+                    </h2>
+                    <div id="cara-mendaftar-cafe" class="accordion-collapse collapse" aria-labelledby="flush-headingThree"
+                        data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">
+                            <ol>
+                                <li>User telah mendaftar akun dan berhasil login,</li>
+                                <li>Setelah itu bisa klik tombol “Mendaftar Café” dipojok kanan atas,</li>
+                                <li>User akan dialihkan menuju halaman daftar cafe, </li>
+                                <li>Disini user bisa memasukkan Nama Café, No Telepon, Alamat café,</li>
+                                <li>Lalu klik “Daftar Café” dan akan muncul halaman profil café,</li>
+                                <li>User bisa mengubah profil café mereka dengan klik “Ubah Profil”, </li>
+                                <li>Disini user bisa menambah, mengedit profil café,</li>
+                                <li>Untuk menambah menu café, user bisa langsung menuju sidebar dibagian kiri dan klik “menu”,</li>
+                                <li>Setelah menuju halaman menu, user bisa memasukan data menu berupa data makanan dan data minuman.
+                                    User
+                                    juga bisa menghapus, menambah dan mengedit data menu.</li>
+                                <li>Untuk menambah tempat user bisa menuju manajemen tempat.</li>
+                            </ol><br />
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#cara-memberikan-ulasan" aria-expanded="false" aria-controls="cara-memberikan-ulasan">
+                            Cara Memberikan Ulasan
+                        </button>
+                    </h2>
+                    <div id="cara-memberikan-ulasan" class="accordion-collapse collapse" aria-labelledby="flush-headingThree"
+                        data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">
+                            <ol style="text-align: left;">
+                                <li>Untuk memberikan Rating dan Ulasan pada café, user harus melakukan login dahulu kedalam web,
+                                </li>
+                                <li>Pilih café yang akan diberikan Rating dan Ulasan,</li>
+                                <li>Scroll kebawah menuju bagian Rating dan Ulasan,</li>
+                                <li>Klik tambah ulasan, lalu akan muncul pop up yang digunakan untuk mengisi Rating dan Ulasan,</li>
+                                <li>Lalu ulasan akan tampil diprofil café,</li>
+                                <li>Perlu diingat, satu akun hanya bisa memberi satu ulasan</li>
+                            </ol><br />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+@endsection
