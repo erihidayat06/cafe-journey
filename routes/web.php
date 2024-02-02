f543347599f9326406084c5fb162bb4050023ca3
andikatuluspangestu <andikatulusp@gmail.com>
2024-02-02 13:00:58 +0700

    Changes : Cafes Route Method

--- routes/web.php
+++ routes/web.php
@@ -62,7 +62,7 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/help', [HomeController::class, 'help']);
 Route::get('/about', [HomeController::class, 'about']);
 Route::get('/contact', [HomeController::class, 'contact']);
-Route::get('/cafes', [HomeController::class, 'cafes']);
+Route::get('/cafes', [HomeController::class, 'cafes'])->name('cafes');
 
 // Cafe
 Route::get('/cafe/{cafe:slug}', [CafeController::class, 'profil']);
