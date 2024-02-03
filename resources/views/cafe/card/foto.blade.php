a6f550231b490840dff1eb4987024d1a32f0f9a9
andikatuluspangestu <andikatulusp@gmail.com>
2024-02-03 13:13:21 +0700

    Fixed : Image Zoom

--- resources/views/cafe/card/foto.blade.php
+++ resources/views/cafe/card/foto.blade.php
@@ -3,13 +3,15 @@
         <div class="card-header">
             <h4 class="text-main ms-2">Gallery</h4>
         </div>
-        <div class="card-body text-center">
+        <div class="card-body text-center d-flex flex-wrap justify-content-center">
             @foreach ($fotos as $foto)
-                <a href="#" data-bs-target="#foto{{ $foto->id }}" data-bs-toggle="modal">
-                    <img style="object-fit: cover" class="mb-3 m-2 shadow-sm rounded-sm"
-                        src="{{ asset('storage/' . $foto->gambar) }}" width="30%" height="200px" alt="">
-                    @include('cafe.modal.modalFoto')
-                </a>
+                <div class="m-2" style="flex: 1; min-width: 200px; max-width: 100%;">
+                    <a href="#" data-bs-target="#foto{{ $foto->id }}" data-bs-toggle="modal">
+                        <img class="rounded-sm" style="object-fit: cover; width: 100%; height: 200px;"
+                            src="{{ asset('storage/' . $foto->gambar) }}" alt="">
+                        @include('cafe.modal.modalFoto')
+                    </a>
+                </div>
             @endforeach
         </div>
     </div>
